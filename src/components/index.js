a5f0937c6cd067e50ac6629258cc95fe2a0b4634
Madhurendra Pratap Singh <142706366+madhuaditya@users.noreply.github.com>
2025-01-21 23:19:11 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from './Header/Header.jsx';
 import Footer from './Footer/Footer.jsx';
-import Container from './Container/Container.jsx'
+import Container from './container/Container.jsx'
 import Logo from './Logo.jsx'
 import Button from './Button.jsx'
 import Select from './Select.jsx'
